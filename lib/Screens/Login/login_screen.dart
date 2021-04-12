1a4539eef387f79f046a0d710013ba7b65d58975
Gustavo <gustavotch@hotmail.com>
2021-04-12 11:43:38 -0300

    Initial Project Commit

---
+++ lib/Screens/Login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Classe para interface da tela de login 
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Login/login_screen.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    title: "Processo Seletivo",
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
